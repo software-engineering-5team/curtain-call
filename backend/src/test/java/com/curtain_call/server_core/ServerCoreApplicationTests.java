9e9d004bd4476e66246a4fdd09f268cb9f6b9f88
yoojanghun <yoojanghun@naver.com>
2026-05-17 22:43:56 +0900

    CI 환경에서는 DB 연결이 없으므로 비활성화"

--- backend/src/test/java/com/curtain_call/server_core/ServerCoreApplicationTests.java
+++ backend/src/test/java/com/curtain_call/server_core/ServerCoreApplicationTests.java
@@ -1,8 +1,10 @@
 package com.curtain_call.server_core;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("CI 환경에서는 DB 연결이 없으므로 비활성화")
 @SpringBootTest
 class ServerCoreApplicationTests {
 
