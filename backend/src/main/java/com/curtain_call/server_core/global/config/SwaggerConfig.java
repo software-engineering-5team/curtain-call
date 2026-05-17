4a3e77b7c8b45b2a42945d9b46b6216b6ce4a710
yoojanghun <yoojanghun@naver.com>
2026-05-17 21:52:44 +0900

    #1 - Swagger 설정

---
+++ backend/src/main/java/com/curtain_call/server_core/global/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.curtain_call.server_core.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwtSchemeName = "jwtAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Curtain Call API")
+                        .description("국민대학교 공연장 대관 및 예매 시스템 API 명세서")
+                        .version("v1.0.0"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
